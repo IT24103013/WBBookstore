08aeb4b84bc15bf1959bf79ee9e948fa3bbcbe74
Samadhi Gunasekara <samadhisavani@gmail.com>
2025-10-21 14:50:34 +0530

    1st commit

---
+++ src/main/java/com/ku_26/webbasedbookstore/WebBasedBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.ku_26.webbasedbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBasedBookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ku_26/webbasedbookstore/WebBasedBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ku_26.webbasedbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBasedBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
